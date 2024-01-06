547e0afc41a2823f464221008f818cf1254c27f4
gabrielteiga <gabrielteiga99@hotmail.com>
2024-01-06 00:12:17 -0300

    Usando cobra para criar a propria CLI

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gabrielteiga/golang-cli-cobra
 
 go 1.16
+
+require github.com/spf13/cobra v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/gabrielteiga/golang-cli-cobra/cmd"
+
+func main() {
+	cmd.Execute()
+}
